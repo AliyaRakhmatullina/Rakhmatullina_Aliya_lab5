7fcb4ed706fb3ac83bd7c6b59a0662bcb9542689
AliyaRakhmatullina <9874208544al@gmail.com>
2024-11-28 23:25:14 +0300

    Добавьте файлы проекта.

---
+++ Rakhmatullina_Aliya_lab5/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Rakhmatullina_Aliya_lab5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Упражнение 6.3
+            //Написать программу, вычисляющую среднюю температуру за год. Создать
+            //двумерный рандомный массив temperature[12, 30], в котором будет храниться температура
+            //для каждого дня месяца(предполагается, что в каждом месяце 30 дней).Сгенерировать
+            //значения температур случайным образом. Для каждого месяца распечатать среднюю
+            //температуру.Для этого написать метод, который по массиву temperature[12, 30] для каждого
+            //месяца вычисляет среднюю температуру в нем, и в качестве результата возвращает массив
+            //средних температур.Полученный массив средних температур отсортировать по
+            //возрастанию.
+            
+            int months = 12;
+            int days = 30;
+            double[,] temperatures = new double[months, days];
+
+            
+            Random rand = new Random();
+            for (int i = 0; i < months; i++)
+            {
+                for (int j = 0; j < days; j++)
+                {
+                    temperatures[i, j] = rand.Next(-40, 41); 
+                }
+            }
+
+            
+            double[] averageTemperatures = CalculateAverageTemperatures(temperatures);
+
+            
+            Console.WriteLine("Средние температуры за каждый месяц:");
+            for (int i = 0; i < months; i++)
+            {
+                Console.WriteLine($"Месяц {i + 1}: {averageTemperatures[i]:F2} °C");
+            }
+
+            
+            Array.Sort(averageTemperatures);
+
+            
+            Console.WriteLine("\nСредние температуры, отсортированные по возрастанию:");
+            foreach (var avgTemp in averageTemperatures)
+            {
+                Console.WriteLine($"{avgTemp:F2} °C");
+            }
+        }
+
+      
+        static double[] CalculateAverageTemperatures(double[,] temperatures)
+        {
+            int months = temperatures.GetLength(0);
+            double[] averageTemperatures = new double[months];
+
+            for (int i = 0; i < months; i++)
+            {
+                double summa = 0;
+
+                for (int j = 0; j < temperatures.GetLength(1); j++)
+                {
+                    summa += temperatures[i, j];
+                }
+
+                averageTemperatures[i] = summa / temperatures.GetLength(1); 
+            }
+
+            return averageTemperatures;
+        }
+
+
+    }
+    
+    
+}
---
+++ Rakhmatullina_Aliya_lab5/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Rakhmatullina_Aliya_lab5")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Rakhmatullina_Aliya_lab5")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("31a9a88a-6887-4c27-814a-35e60c7d57ed")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
